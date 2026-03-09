83ecd1a11b50d1675b8b6fcd08244aff12d03071
Rabiya <1khan.rabiya@gmail.com>
2026-03-09 16:28:36 +0530

    Remove files

--- feature_engineering.py
+++
@@ -1,9 +0,0 @@
-def create_labels(data):
-
-    threshold = data['Total reported direct emissions'].median()
-
-    data['compliance'] = (
-        data['Total reported direct emissions'] > threshold
-    ).astype(int)
-
-    return data
