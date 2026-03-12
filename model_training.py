74fdd0b3934a0d62d0688a5eda611ad3d03bd73c
santoshelde <ldsantosh@hotmail.com>
2026-03-12 14:49:15 +0530

    Implemented GridsearchCv
    Implemented GridsearchCb to the model_training.py program, this here is used to find out the best paramaters for our dataset

--- model_training.py
+++ model_training.py
@@ -1,18 +1,29 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 scaler = StandardScaler()
-model = RandomForestClassifier(random_state=42)
 
 def train_model(X_train, y_train):
-
-    
     X_train_scaled = scaler.fit_transform(X_train)
 
-    
-    model.fit(X_train_scaled, y_train)
+    rf = RandomForestClassifier(random_state=42)
 
-    print("Model training completed")
-    print(model)
+    param_grid = {
+        'n_estimators': [50, 100],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5]
+    }
 
-    return model, scaler
+
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, scoring='accuracy')
+
+    print("Starting Grid Search... please wait.")
+    grid_search.fit(X_train_scaled, y_train)
+
+    print("\n" + "="*30)
+    print("BEST PARAMETERS FOUND:")
+    print(grid_search.best_params_)
+    print("="*30 + "\n")
+
+    return grid_search.best_estimator_, scaler
